b5f16274c2b6ad4b8df742dff8392dbbaba03506
I7RANK <franfgh2000@hotmail.com>
2022-01-09 21:40:56 -0500

    Creating Header and main navbar

---
+++ src/Header/header.js
@@ -0,0 +1,25 @@
+import './header.css'
+
+function Header() {
+    return (
+        <header>
+            <span>ShareLocation</span>
+            <div className='content-menu'>
+                <input id='navbar-control' type={'checkbox'}></input>
+                <label className='icon-menu' htmlFor='navbar-control'></label>
+                <label className='label-btn' htmlFor='navbar-control'></label>
+
+
+                <nav className='main-navbar'>
+                    <ul>
+                        <li>Contact me</li>
+                        <li>About ShareLocation</li>
+                        <li>GitHub repo</li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
