625ef05f094eb09729d3ccbea4975259b4918736
I7RANK <franfgh2000@hotmail.com>
2022-02-17 06:46:57 -0500

    Adding Home description component

---
+++ src/pages/Home/Description/description.js
@@ -0,0 +1,13 @@
+import './description.css';
+
+function Description() {
+  return (
+    <>
+      <p className='description'>
+        ShareLocation allows to the users to share his current location to other people in real time.
+      </p>
+    </>
+  );
+}
+
+export default Description;
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -1,10 +1,12 @@
 import StartButton from "./StartButton/startButton";
 import Banner from "./Banner/banner";
+import Description from "./Description/description";
 
 function Home() {
   return (
     <>
       <Banner />
+      <Description />
     </>
   );
 }
